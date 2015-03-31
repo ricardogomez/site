1a2d3d7502aea5e90a993a39c10a6b779ddc810c
danigb <danigb@gmail.com>
2015-04-01 01:49:50 +0200

    Redirects

---
+++ app/plugins/redirects.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var templ = require('../lib/templ');
+var fs = require('fs');
+
+module.exports = plugin;
+
+var t = '<!DOCTYPE html>' +
+'<html>' +
+  '<head>' +
+    '<meta charset="utf-8">' +
+    '<meta http-equiv="refresh" content="1;url={{destination}}">' +
+    '<link rel="canonical">' +
+    '<script>window.location.replace("{{destination}}");</script>' +
+  '</head>' +
+  '<body>Esta página se movió <a href="{{destination}}">aquí.</a>' +
+  '</body>' +
+'</html>';
+
+function plugin() {
+  return function(files, metalsmith, done) {
+    var file = files['redirects.txt'];
+    var lines = file.contents.toString().split('\n');
+    console.log("REDIRECT", file, lines.length);
+    lines.forEach(function(line) {
+      var parts = line.replace(/\s/g, '').split(':');
+      var original = parts[0].replace(/\"/g, '').substring(1);
+      var source = original + '/index.html'
+      var dest = parts[1];
+      if (dest) {
+        var redirect = templ(t, function() { return "/" + dest; })
+        files[source] = {
+          contents: new Buffer(redirect)
+        }
+      }
+    });
+    done();
+  }
+}
