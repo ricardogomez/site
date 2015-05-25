7808b4d2f1a6931056f891f955801f32d5d0051c
danigb <danigb@gmail.com>
2015-05-25 13:02:02 +0200

    Pull before load server

--- code/app/start.js
+++ code/app/start.js
@@ -3,11 +3,11 @@
 var Metalsmith = require('./lib/metalsmith-cli.js');
 var chalk = require('chalk');
 var path = require('path');
+var simpleGit = require('simple-git')
 
 console.log(chalk.green("Hola papá!"));
 
 var site = path.join(__dirname, '../site');
-
 var metalsmith = Metalsmith(site);
 
 metalsmith.build(function(err) {
@@ -18,12 +18,17 @@ metalsmith.build(function(err) {
   console.log(chalk.green("Web lista."));
 });
 
-if ("server") {
-  console.log(chalk.yellow("Iniciando servidor..."));
-  var server = require('./lib/server.js')
-  server.start({
-    "metalsmith": metalsmith,
-  });
-}
+console.log(chalk.grey('Sincronizando datos...'));
+var git = simpleGit(path.join(__dirname, '../..'));
+git.pull(function() {
+  console.log(chalk.grey('Datos listos.'));
+  if ("server") {
+    console.log(chalk.yellow("Iniciando servidor..."));
+    var server = require('./lib/server.js')
+    server.start({
+      "metalsmith": metalsmith,
+    });
+  }
 
-console.log("Pulsa Ctrl+C para parar.")
+  console.log("Pulsa Ctrl+C para parar.")
+});
